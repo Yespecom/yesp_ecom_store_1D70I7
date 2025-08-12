ac2837142853a62f556003fb3b897d21065777d0
Yespecom <yespecomstudio@gmail.com>
2025-08-12 19:50:23 +0530

    Update page.tsx

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -132,7 +132,7 @@ export default function ProductsPage() {
 
   return (
     <div className="min-h-screen bg-gray-50">
-      <Header />
+      
 
       <div className="container mx-auto px-4 py-8">
         {/* Page Header */}
